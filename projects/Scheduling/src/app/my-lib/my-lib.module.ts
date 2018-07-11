27a26f66e711323935e2cbf3c93f9634086a13fc
LAPTOP-CNQFB4JI\pikom <piko.mac@gmail.com>
2018-07-12 01:32:13 +0900

    added data-table component

--- projects/Scheduling/src/app/my-lib/my-lib.module.ts
+++ projects/Scheduling/src/app/my-lib/my-lib.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataTableComponent } from './data-table/data-table.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [DataTableComponent]
 })
 export class MyLibModule { }
