8bc56c20cf58e5167539f9d8ca68a0dfcf34c48f
Hideaki-SIC-MacBook <noshiro.pf@gmail.com>
2018-08-07 15:05:10 +0900

    .

--- projects/Scheduling/src/app/scheduling/scheduling-event.ts
+++ projects/Scheduling/src/app/scheduling/scheduling-event.ts
@@ -4,27 +4,36 @@ import { Answer } from './answer';
 
 
 export class SchedulingEvent {
-  databaseKey:       string     = '';
-  title:             string     = '';
-  notes:             string     = '';
-  selectedDatetimes: Date[]     = [];
-  answerDeadline:    Date       = new Date();
+  databaseKey:       string             = '';
+  title:             string             = '';
+  notes:             string             = '';
+  selectedDatetimes: Date[]             = [];
+  answerDeadline:    Date               = new Date();
   symbols:           SchedulingSymbol[] = [];
-  answers:           Answer[]   = [];
-  password:          string     = '';
+  answers:           Answer[]           = [];
+  password:          string             = '';
 
   constructor( databaseKey?: string, initObj?: SchedulingEventInitObj ) {
     this.databaseKey = ( databaseKey || '' );
 
     if ( !initObj ) return;
+
     this.title = ( initObj.title || '' );
     this.notes = ( initObj.notes || '' );
-    this.selectedDatetimes = ( initObj.selectedDatetimesTimeStamps || [] )
-                                .map( e => new Date( e || 0 ) );
-    this.answerDeadline = new Date( initObj.answerDeadlineTimeStamp || 0 );
+
+    this.selectedDatetimes
+      = ( initObj.selectedDatetimesTimeStamps || [] )
+          .map( e => new Date( e || 0 ) );
+
+    this.answerDeadline
+      = new Date( initObj.answerDeadlineTimeStamp || 0 );
+
     this.symbols = ( initObj.symbols || [] );
-    this.answers = ( utils.object.entries( initObj.answers )
-                      .map( e => new Answer( e.key, e.value ) ) || [] );
+
+    this.answers
+      = ( utils.object.entries( initObj.answers )
+          .map( e => new Answer( e.key, e.value ) ) || [] );
+
     this.password = ( initObj.password || '' );
   }
 }
