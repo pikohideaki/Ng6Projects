e7e8827b4aefb7298216e92204441de37e697f51
Hideaki <noshiro.pf@gmail.com>
2018-09-06 11:06:10 +0900

    add files to ScheduleApp1

--- projects/ScheduleApp1/src/app/app.module.ts
+++ projects/ScheduleApp1/src/app/app.module.ts
@@ -3,14 +3,51 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+
+/* angular material */
+import { MyOwnCustomMaterialModule } from './my-own-custom-material.module';
+
+/* ngx-pipes */
+import { NgPipesModule } from 'ngx-pipes';
+
+/* Feature Modules */
+import { ClipboardModule } from 'ngx-clipboard';
+
+/* my modules & components */
+import { MyLibModule } from './mylib/mylib';
+
+/* angularfire2 */
+import { MyOwnAngularFireModule } from './database/my-own-angular-fire.module';
+
+import { HomeComponent } from './home.component';
+import { NotFoundPageComponent } from './not-found-page.component';
+import { FeedbackAdminComponent } from './feedback/feedback-admin.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+
+import { PikoAppsModule } from './piko-apps/piko-apps.module';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundPageComponent,
+    FeedbackComponent,
+    FeedbackAdminComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClipboardModule,
+    NgPipesModule,
+    AppRoutingModule,
+    MyOwnCustomMaterialModule,
+    MyLibModule,
+    MyOwnAngularFireModule,
+    PikoAppsModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
